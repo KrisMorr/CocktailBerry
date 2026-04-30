eb5405985c0664943bf6deb2bb736e42992b5f68
KrisMorr <154343071+KrisMorr@users.noreply.github.com>
2026-04-30 11:42:14 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from typing import Literal
 __version__ = "4.0.0"
 PROJECT_NAME = "CocktailBerry"
 MAX_SUPPORTED_BOTTLES = 24
-SupportedLanguagesType = Literal["en", "de"]
+SupportedLanguagesType = Literal["en", "de", "pl"]
 SupportedThemesType = Literal["default", "bavaria", "alien", "berry", "tropical", "purple", "custom"]
 SupportedRfidType = Literal["MFRC522", "USB"]
 SupportedPaymentOptions = Literal["Disabled", "CocktailBerry", "SumUp"]
